2531890fa23e78484e8e49a0015b976ceaf6f1a0
Arnaud Ouvrier <arnaud.ouvrier@gmail.com>
2017-09-26 21:39:20 +0200

    initial commit

---
+++ src/js/animate.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+	var bodyHeight = $(document).height();
+
+	console.log(bodyHeight);
+
+	$(document).scroll(function() {
+		
+		var scrollPos = $(document).scrollTop();
+		var documentHeight = $(document).height();
+		var windowHeight = $(window).height();
+
+		var scrollAchieved = Math.round(scrollPos/(documentHeight-windowHeight)*1000)/10;
+
+		$('.scroll-indicator-bar').width(scrollAchieved + '%');
+	});
+
+	// var parallaxHeight = 0.9 * $(window).height() - $('#home-parallax').offset().top;
+	// $('#home-parallax').height(parallaxHeight + 'px');
+	// console.log(parallaxHeight);
+
+	$("#navbar a[href^='#']").click(function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top - $('body').data('offset')
+     }, 750, 'swing', function(){
+       window.location.hash = hash;
+     });
+
+});
+})
