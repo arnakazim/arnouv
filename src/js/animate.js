d6ae1c2a87bb52a6d6947f4cc53682e63d7cdc15
Arnaud Ouvrier <arnaud.ouvrier@gmail.com>
2017-09-26 23:48:20 +0200

    Remove useless js

--- src/js/animate.js
+++ src/js/animate.js
@@ -1,35 +1,12 @@
 $(document).ready(function(){
-	var bodyHeight = $(document).height();
-
-	console.log(bodyHeight);
-
-	$(document).scroll(function() {
-		
-		var scrollPos = $(document).scrollTop();
-		var documentHeight = $(document).height();
-		var windowHeight = $(window).height();
-
-		var scrollAchieved = Math.round(scrollPos/(documentHeight-windowHeight)*1000)/10;
-
-		$('.scroll-indicator-bar').width(scrollAchieved + '%');
-	});
-
-	// var parallaxHeight = 0.9 * $(window).height() - $('#home-parallax').offset().top;
-	// $('#home-parallax').height(parallaxHeight + 'px');
-	// console.log(parallaxHeight);
-
 	$("#navbar a[href^='#']").click(function(e) {
+		e.preventDefault();
+		var hash = this.hash;
 
-   // prevent default anchor click behavior
-   e.preventDefault();
-
-   // store hash
-   var hash = this.hash;
-
-   // animate
-   $('html, body').animate({
-       scrollTop: $(hash).offset().top - $('body').data('offset')
-     }, 750, 'swing', function(){});
-
-});
+		if(hash.length > 1) {
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top - $('body').data('offset')
+			}, 750, 'swing', function(){});
+		}
+	});
 })
