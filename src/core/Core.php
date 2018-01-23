b0188fd4f199ffb11438c950c3dbe6090c979440
Arnaud Ouvrier <arnaudo@nutrimetics.fr>
2018-01-23 16:56:07 +0100

    Better content management

--- src/core/Core.php
+++ src/core/Core.php
@@ -6,150 +6,172 @@
 
 namespace Core;
 
-class Core {
+class Core
+{
 
-	private $conf;
-	private $lang;
+    private $conf;
+    private $lang;
 
-	private $page;
-	private $template;
+    private $page;
+    private $template;
 
-	private $getContent;
-	private $postContent;
-	
-	public function __construct() {
-		$confFile = APP . DS . 'json' . DS . 'conf.json';
+    private $getContent;
+    private $postContent;
+    
+    public function __construct()
+    {
+        $confFile = APP . DS . 'json' . DS . 'conf.json';
 
-		if(!file_exists($confFile)) {
-			echo 'Configuration file not found.';
-			die();
-		}
+        if (!file_exists($confFile)) {
+            echo 'Configuration file not found.';
+            die();
+        }
 
-		// Load configuration
-		$this->conf = json_decode(file_get_contents($confFile));
+        // Load configuration
+        $this->conf = json_decode(file_get_contents($confFile));
 
-		// Get get and post vars
-		$this->getContent = new \stdClass();
-		$this->postContent = new \stdClass();
+        // Get get and post vars
+        $this->getContent = new \stdClass();
+        $this->postContent = new \stdClass();
 
-		foreach ($_GET as $k => $v)
-			$this->getContent->$k = $v;
+        foreach ($_GET as $k => $v) {
+            $this->getContent->$k = $v;
+        }
 
-		foreach ($_POST as $k => $v)
-			$this->postContent->$k = $v;
+        foreach ($_POST as $k => $v) {
+            $this->postContent->$k = $v;
+        }
 
-		// Set language
-		$this->lang = $this->conf->languages->default;
-		if(!empty($this->getContent->lang) && in_array($this->getContent->lang, $this->conf->languages->available))
-			$this->lang = $this->getContent->lang;
-			
+        // Set language
+        $this->lang = $this->conf->languages->default;
+        if (!empty($this->getContent->lang) && in_array($this->getContent->lang, $this->conf->languages->available)) {
+            $this->lang = $this->getContent->lang;
+        }
+            
 
-		// Define page to load
-		$this->page = $this->conf->templates->defaultPage;
-		if(!empty($this->getContent->page)) {
-			if(!file_exists(JSON . DS . "pages" . DS . $this->getContent->page . '.json')) {
-				echo 'Page file not found.';
-				die();
-			}
-			$this->page = $this->getContent->page;
-		}
-			
-		// Define templaate to use
-		$this->template =  $this->conf->templates->defaultTemplate;
-		if(!empty($this->page->conf->template) && in_array($this->page->conf->template, $this->conf->template->available)){
-			if(!file_exists(JSON . DS . "templates" . DS . $this->page->conf->template . DS . 'template.html')) {
-				echo 'Template not found.';
-				die();
-			}
-			$this->page = $this->getContent->page;
-		}
+        // Define page to load
+        $this->page = $this->conf->templates->defaultPage;
+        if (!empty($this->getContent->page)) {
+            if (!file_exists(JSON . DS . "pages" . DS . $this->getContent->page . '.json')) {
+                echo 'Page file not found.';
+                die();
+            }
+            $this->page = $this->getContent->page;
+        }
+            
+        // Define template to use
+        $this->template =  $this->conf->templates->defaultTemplate;
+        if (!empty($this->page->conf->template)
+            && in_array($this->page->conf->template, $this->conf->template->available)
+        ) {
+            if (!file_exists(JSON . DS . 'templates' . DS . $this->page->conf->template . DS . 'template.html')) {
+                echo 'Template not found.';
+                die();
+            }
+            $this->page = $this->getContent->page;
+        }
 
-		$this->render();
-	}
+        $this->render();
+    }
 
-	private function render() {
-		$templateFile = file_get_contents(APP . DS . "templates" . DS . $this->template . DS . 'template.html');
+    private function render()
+    {
+        $templateFile = file_get_contents(APP . DS . 'templates' . DS . $this->template . DS . 'template.html');
 
-		$templateFile = str_replace('%{(lang)}%', $this->lang, $templateFile);
+        $templateFile = str_replace('%{(lang)}%', $this->lang, $templateFile);
 
-		foreach($this->conf->attributes as $name => $value) {
-			$templateFile = str_replace('%{' . $name . '}%', $value->{$this->lang}, $templateFile);
-		}
+        foreach ($this->conf->attributes as $name => $value) {
+            $templateFile = str_replace('%{' . $name . '}%', $value->{$this->lang}, $templateFile);
+        }
 
-		if(preg_match_all('/%{\[date\(\'([a-zA-Z\-\/\:\ ]+)\'\)\]}%/', $templateFile, $matches)) {
-			foreach($matches[0] as $i => $match) {
-				$templateFile = str_replace($match, date($matches[1][$i]), $templateFile);
-			}
-		}
+        if (preg_match_all('/%{\[date\(\'([a-zA-Z\-\/\:\ ]+)\'\)\]}%/', $templateFile, $matches)) {
+            foreach ($matches[0] as $i => $match) {
+                $templateFile = str_replace($match, date($matches[1][$i]), $templateFile);
+            }
+        }
 
-		$pageDefinition = json_decode(file_get_contents(JSON . DS . "pages" . DS . $this->page . '.json'));
+        $pageDefinition = json_decode(file_get_contents(JSON . DS . "pages" . DS . $this->page . '.json'));
+        
+        $contentForTemplate = $this->generateCode($pageDefinition->contents, $pageDefinition->conf ?? null);
 
-		$contentForTemplate = $this->prepareContent(clone $pageDefinition);
+        $templateFile = str_replace('%{(content_for_template)}%', $contentForTemplate, $templateFile);
 
-		$templateFile = str_replace('%{(content_for_template)}%', $contentForTemplate, $templateFile);
+        echo $templateFile;
+    }
 
-		echo $templateFile;
-	}
+    private function generateCode($contents, $conf)
+    {
+        $generatedFile = '';
+        foreach ($contents as $content) {
+            $contentFilePath = APP.DS . 'templates' . DS . $this->template . DS . 'contents' . DS . $content->type . '.html';
+            $contentGenerated = '';
 
-	private function prepareContent($json) {
-		$conf = $json->conf;
-		unset($json->conf);
+            if (file_exists($contentFilePath)) {
+                $contentFile = \file_get_contents($contentFilePath);
 
-		$generatedFile = '';
-		
-		foreach($json as $type => $content) {
-			
-			$generatedFile .= $this->generateCode($content, null, $type);
+                foreach ($content as $property => $value) {
+                    if ($property == 'contents') {
+                        $value = $this->generateCode($value, $conf);
+                    }
 
-			// $contentFilePath = APP . DS . "templates" . DS . $this->template . DS . $type . DS . 'main.html';
-			// if(file_exists($contentFilePath)) {
-			// 	$contentFile = \file_get_contents($contentFilePath);
+                    if (is_object($value)) {
+                        if (isset($value->type)) {
+                            switch ($value->type) {
+                                case "text":
+                                    $value = $value->{$this->lang};
+                                    break;
+                                case "conf":
+                                    $value = $conf->{$value->property}->{$value->value};
+                                    break;
+                                default:
+                                    $value = null;
+                            }
+                        } else {
+                            $value = null;
+                        }
+                    }
+                    
+                    $contentFile = str_replace('%[' . $property . ']%', $value, $contentFile);
+                }
 
-			// 	foreach($contents as $name => $content){
-			// 		$tempContent = str_replace('%[name]%', $name, $contentFile);
+                $generatedFile .= $contentFile;
+            }
+        }
 
-			// 		$generatedFile .= str_replace('%[content]%', '', $tempContent);
-			// 	}
+        return $generatedFile;
+    }
 
-			// }
-		}
+    // private function generateCode($content, $subpath = null, $type = null)
+    // {
+    //     $contentFilePath = APP.DS . 'templates' . DS . $this->template . DS . $subpath . ($type ?? $content->type) . DS . 'main.html';
 
-		return $generatedFile;
-	}
+    //     $generatedFile = '';
 
-	private function generateCode($content, $subpath = null, $type = null) {
-		$contentFilePath = APP . DS . "templates" . DS . $this->template . DS . $subpath . ($type ?? $content->type) . DS . 'main.html';
+    //     if (file_exists($contentFilePath)) {
+    //         $contentFile = \file_get_contents($contentFilePath);
 
-		$generatedFile = '';
+    //         foreach ($content as $block) {
+    //             debug($block);
+    //             $blockType = $block->type;
 
-		if(file_exists($contentFilePath)) {
-			$contentFile = \file_get_contents($contentFilePath);
+    //             foreach ($block as $name => $value) {
+    //                 if ($name == 'content') {
+    //                     // Call to itself
+    //                     $subContent = $this->generateCode(
+    //                         $value,
+    //                         $subpath . ((!is_null($blockType))?DS . $blockType:'') . DS
+    //                     );
 
-			foreach($content as $block){
-				debug($block);
-				$blockType = $block->type;
+    //                     $contentFile = str_replace('%[content]%', $subContent, $contentFile);
+    //                 } else {
+    //                     $contentFile = str_replace('%[' . $name . ']%', $value, $contentFile);
+    //                 }
+    //             }
+    //         }
+    //         debug($generatedFile);
+    //         $generatedFile .= $contentFile;
+    //     }
 
-				foreach($block as $name => $value) {
-
-					if($name == 'content') {
-						// Call to itself
-						$subContent = $this->generateCode($value, $subpath . ((!is_null($blockType))?DS . $blockType:'') . DS);
-						$contentFile = str_replace('%[content]%', $subContent, $contentFile);
-					}
-					else {
-						$contentFile = str_replace('%[' . $name . ']%', $value, $contentFile);
-					}
-				}
-
-			}
-			debug($generatedFile);
-			$generatedFile .= $contentFile;
-
-		}
-
-		return $generatedFile;
-	}
-
+    //     return $generatedFile;
+    // }
 }
-
-?>
