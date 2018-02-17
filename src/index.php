14b26232f4e9270fcfe113135a68b92d05480a35
Arnakazim <arnaud.ouvrier@gmail.com>
2018-02-17 18:52:43 +0700

    Improve engine
    TODO: 404 managment

--- src/index.php
+++ src/index.php
@@ -1,26 +1,38 @@
 <?php
 
-$availableLanguages = array(
-    "fr_FR",
-    "en_US"
-);
+$lang = 'en_US'; // Default language
+$langDir = 'json' . DIRECTORY_SEPARATOR . 'lang';
 
-$lang = 'en_US';
+$page = 'index'; // default page
+$pageDir = 'html';
+
+// if lang is set in the get request, we overide the default lang value
 if (isset($_GET['lang'])) {
+    // List all avalailable languages available in the json/lang directory
+    $availableLanguages = array();
+    $langFiles = array_diff(scandir($langDir), array('..', '.'));
+
+    foreach($langFiles as $k => $v) {
+        if(substr($v, -5) === '.json') {
+            $availableLanguages[] = substr($v, 0, -5);
+        }
+    }
+
+    // if language is available
     if (in_array($_GET['lang'], $availableLanguages)) {
         $lang = $_GET['lang'];
     }
 }
 
-$langFile = 'json' . DIRECTORY_SEPARATOR . 'lang' . DIRECTORY_SEPARATOR . $lang . '.json';
-$strings = json_decode(file_get_contents($langFile));
+$langFile = $langDir . DIRECTORY_SEPARATOR . $lang . '.json';
+$strings = json_decode(file_get_contents($langFile)); // get the srings defined in the lang file
 
-$page = 'index';
+// if page is set in the request
 if (isset($_GET['page'])) {
     $page = $_GET['page'];
 }
 
-$pageHtml = file_get_contents($page . '.html');
+$pageHtml = file_get_contents($pageDir. DIRECTORY_SEPARATOR . $page . '.html');
 
 foreach ($strings as $name => $value) {
     $pageHtml = str_replace('%{' . $name . '}%', $value, $pageHtml);
